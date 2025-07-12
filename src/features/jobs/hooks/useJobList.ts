6aef46a356e2ec4e2e333d7b57ea23c430b8dae2
Suleyman Laarabi <suleyman.laarabi@bleemeo.com>
2025-07-12 15:50:39 +0200

    feat: refactor useJobList to use useAtom for period state management

--- src/features/jobs/hooks/useJobList.ts
+++ src/features/jobs/hooks/useJobList.ts
@@ -1,14 +1,15 @@
-import { useAtomValue, useSetAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { jobsAtom, jobSelectedPeriodAtom } from "../atoms";
 import type { JobModel } from "backend/modules/job/model";
 
 export default function useJobList() {
   const { data, isLoading } = useAtomValue(jobsAtom);
-  const setPeriod = useSetAtom(jobSelectedPeriodAtom);
+  const [period, setPeriod] = useAtom(jobSelectedPeriodAtom);
   return {
+    period,
+    setPeriod,
     jobs: data?.data || [],
     isLoading,
-    setPeriod,
   };
 }
 
